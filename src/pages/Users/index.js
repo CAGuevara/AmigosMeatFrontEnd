ab6237273131b9d9fb9b2e174d49d248f7b86fd0
Carmen Guevara <carmen.guevara75@gmail.com>
2022-03-08 10:21:02 -0500

    updating User and Products access

--- src/pages/Users/index.js
+++ src/pages/Users/index.js
@@ -1,10 +1,30 @@
 import React from 'react';
 import './styles.css';
 import AddUsers from '../addusers';
-
+import { useEffect, useState } from 'react';
+import axios from 'axios';
 
 const Users = () => {
+    const [users, setUsers] = useState([]);
+
+    useEffect(()=> {
+        fetchUSers()
+    },[]);
+
+    const fetchUSers = async() => {
+        try {
+            const response = await axios.get('http://localhost:8080/api/v1/allusers');
+            console.log(response)
+            setUsers(response.data)
+
+        }catch(error){
+            console.log(error);
+        }
+    }
+
     return (
+        <>
+            
            <div className="ui centered grid vertical fluid menu" id="users-menu">
 
                     <div className="ui vertical animated button" tabindex="0">
@@ -19,7 +39,9 @@ const Users = () => {
                         <div className="hidden content"> Modificar Usuarios </div>
                         <div className="visible content">Update Users</div>
                     </div>
-        </div>
+
+            </div>
+        </>
     );
 }
 
